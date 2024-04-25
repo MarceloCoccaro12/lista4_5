1598f24e1e479ffc71d8f59a6711e8cc360cc7dc
Marcelo <marcelococcaro@email.com>
2024-04-24 22:19:41 -0300

    exer5_1

---
+++ exer5_1.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    v1=int(input("Digite um valor:"))
+    v2=int(input("Digite um valor:"))
+
+    soma= v1 + v2
+
+    if (v1 > v2):
+        print("V1 é maior")
+    else:
+        print("V2 é maior")
