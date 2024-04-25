582712633052929a9344a9300c4af7386a8237e7
Marcelo <marcelococcaro@email.com>
2024-04-24 22:18:07 -0300

    exer4_2

---
+++ exer4_2.py
@@ -0,0 +1,10 @@
+n1= int(input("Digite um número:"))
+n2= int(input("Digite um número:"))
+n3= int(input("Digite um número:"))
+n4= int(input("Digite um número:"))
+n5= int(input("Digite um número:"))
+n6= int(input("Digite um número:"))
+
+lista=[n1, n2, n3, n4, n5, n6]
+lista.sort()
+print(lista)
